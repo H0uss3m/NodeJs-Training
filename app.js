0091aff5c777024b0d02eb12824c89e001256704
Houssem <h.benmustpha@ewm.swiss>
2023-03-08 15:01:17 +0100

    update one

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get("/api/stuff", (req, res, next) => {
     .then((things) => res.status(200).json(things))
     .catch((error) => res.status(400).json({ error }));
 });
+
 app.post("/api/stuff", (req, res, next) => {
   delete req.body._id;
   const newThing = new Thing({ ...req.body });
@@ -47,9 +48,15 @@ app.post("/api/stuff", (req, res, next) => {
     })
     .catch((error) => res.status(400).json({ error }));
 });
+
 app.get("/api/stuff/:id", (req, res, next) => {
   Thing.findById(req.params.id)
     .then((thing) => res.status(200).json(thing))
     .catch((error) => res.status(404).json({ error }));
+});
+app.put("/api/stuff/:id", (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then((thing) => res.status(200).json({ message: "objet modifier" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 module.exports = app;
