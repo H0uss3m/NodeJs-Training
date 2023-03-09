e41d9a103429e81975f4ba99a651c717396b9d24
Houssem <h.benmustpha@ewm.swiss>
2023-03-09 14:33:18 +0100

    create singup controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,21 @@
-exports.signup = (req,res,next)=>{
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-}
-exports.login = (req,res,next)=>{
+exports.signup = (req, res, next) => {
+  const { password, email } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => {
+      const user = new User({
+        email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "user created !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
-}
+exports.login = (req, res, next) => {};
